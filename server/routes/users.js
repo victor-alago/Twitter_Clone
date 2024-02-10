d57e83d35b3eebdff5f6cb0f1a3d3d034acd52ea
ductinnguyen <127945019+Duc-Tin-Nguyen@users.noreply.github.com>
2024-02-10 23:29:03 +0100

    Implement user update endpoint with authentication

---
+++ server/routes/users.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  getUser,
+  update,
+  deleteUser,
+  follow,
+  unFollow,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+// Update User
+router.put("/:id", verifyToken, update);
