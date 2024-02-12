e92b929133c0e0433b9266e577ba090418befad2
Alago Victor <123035877+victor-alago@users.noreply.github.com>
2024-02-13 00:11:13 +0100

    Added the routes for tweets

---
+++ server/routes/tweets.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { verifyToken } from "../verifyToken.js";
+import { createTweet, deleteTweet, likeOrDislikeTweet, getAllTweets,getUserTweets, getExploreTweets } from "../services/tweet.js";
+
+
+const router = express.Router();
+
+
+router.post('/', verifyToken, createTweet);
+
+
+router.delete('/:id', verifyToken, deleteTweet);
+
+
+//like or dislike a tweet
+router.put('/:id/like', verifyToken, likeOrDislikeTweet);
+
+//get all timeline tweets
+router.get('/timeline/:id', getAllTweets);
+
+//get only the user's tweets
+router.get('/user/all/:id', getUserTweets);
+
+
+//explore, contains all tweets from all users, most liked tweets, most retweeted tweets
+router.get('/explore', getExploreTweets); //trending tweets
+
+export default router;
