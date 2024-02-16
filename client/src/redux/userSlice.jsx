952911488a37d57bff514552584a690a22ea42c2
ductinnguyen <127945019+Duc-Tin-Nguyen@users.noreply.github.com>
2024-02-16 16:17:49 +0100

    Defined userSlice with reducers for user state management

--- client/src/redux/userSlice.jsx
+++ client/src/redux/userSlice.jsx
@@ -5,3 +5,38 @@ const initialState = {
   isLoading: false,
   error: false,
 };
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+      loginStart: (state) => {
+        state.isLoading = true;
+      },
+      loginSuccess: (state, action) => {
+        state.isLoading = false;
+        state.currentUser = action.payload;
+      },
+      loginFailed: (state) => {
+        state.isLoading = false;
+        state.error = true;
+      },
+      logout: (state) => {
+        return initialState;
+      },
+      changeProfile: (state, action) => {
+        state.currentUser.profilePicture = action.payload;
+      },
+      following: (state, action) => {
+        if (state.currentUser.following.includes(action.payload)) {
+          state.currentUser.following.splice(
+            state.currentUser.following.findIndex(
+              (followingId) => followingId === action.payload
+            )
+          );
+        } else {
+          state.currentUser.following.push(action.payload);
+        }
+      },
+    },
+  });
