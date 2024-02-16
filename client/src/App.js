59ffb3d2681010e8aeaef5faec3dc43eae55b6ae
Alago Victor <123035877+victor-alago@users.noreply.github.com>
2024-02-16 16:23:45 +0100

    Completed the App and Index for the client to work

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,73 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Explore from "./pages/Explore/Explore";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Messages from "./pages/Messages/Messages";
+import Navbar from "./components/Navbar/Navbar";
+import Error from "./pages/Error/Error";
+import "./App.css";
+import Tweet from "./pages/Tweet/Tweet";
+
+// create a theme for the website
+const Layout = () => {
+  return (
+    <div className="md:w-8/12 mx-auto">
+      <Navbar />
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+// create router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <Error />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/profile/:username",
+        element: <Profile />,
+      },
+      {
+        path: "/explore",
+        element: <Explore />,
+      },
+      {
+        path: "/tweets/find/:id",
+        element: <Tweet />,
+      },
+      {
+        path: "/messages",
+        element: <Messages />,
+      },
+      {
+        path: "/logout",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+// persist from redux is used to prevent loss of authenticated user data when the page is refreshed.
+import { persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate> 
+    </Provider>
   </React.StrictMode>
 );
 
