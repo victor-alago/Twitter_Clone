0ea2352d335fde916ed87f5dbd41ce97de65c440
ductinnguyen <127945019+Duc-Tin-Nguyen@users.noreply.github.com>
2024-02-16 15:04:39 +0100

    Implementing signup form with state management using React hooks

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -4,3 +4,13 @@ import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginStart, loginSuccess, loginFailed} from '../../redux/userSlice';
+
+const Signup= () => {
+    // state management
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    
