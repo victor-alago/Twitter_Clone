d1e25ac41281bb133b1922a875419505537cf298
ven2604 <khanhnguyen26403@gmail.com>
2024-02-16 16:26:41 +0100

    Signup function in auth services

--- server/services/auth.js
+++ server/services/auth.js
@@ -4,27 +4,53 @@ import jwt from "jsonwebtoken";
 import { handleError } from "../error.js";
 
 
-export const signup = async (req, res, next) => {
-    try {
-      const salt = bcrypt.genSaltSync(10);
-      const hash = bcrypt.hashSync(req.body.password, salt);
-      const newUser = new User({ ...req.body, password: hash });
-  
-      await newUser.save();
-  
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT);
-  
-      const { password, ...othersData } = newUser._doc;
-      res
-        .cookie("access_token", token, {
-          httpOnly: true,
-        })
+const signup =  async (req, res) => {
+    // get user data from request body
+    const {firstname, lastname, username, email, password} = req.body;
+
+    // check if all required fields are submitted
+    if (!firstname || !lastname || !username || !email || !password){
+        return res.status(400).json({error: "missing information"});
+    };
+
+    // check if user already exists
+    const user = await User.findOne({email: email});
+    if (user){
+        return res.status(400).json({error: "User already exists, please login."})
+    };
+
+    // hash password
+    const passwordHash = CryptoJS.AES.encrypt(
+        password, process.env.SECRET_KEY
+    ).toString();
+
+    try{
+        // create new user
+        const newUser = new User({firstname, lastname, username, email, password: passwordHash});
+        // save user and return response
+        const user = await newUser.save();
+
+        // create jwt token
+        const token = jwt.sign(
+            {id:user._id, username:user.username},
+            process.env.SECRET_KEY,
+            {expiresIn: "2h"});
+
+        // destructure user object to remove password
+        const {password, ...info} = user._doc;
+        // console.log(info);
+        
+        // return response with status, user info, access token and cookie
+        return res
+        .cookie("token", token, {httpOnly: true})
         .status(200)
-        .json(othersData);
-    } catch (err) {
-      next(err);
+        .json({...info, token});
+s
+    } catch(err){
+        console.log(err)
+        return res.status(500).json({error: "Failed to save user."})
     }
-  };
+};
   
   
 
