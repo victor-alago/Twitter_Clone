515c84c861415309de834efcef64aa6a520e5029
ven2604 <khanhnguyen26403@gmail.com>
2024-02-17 12:07:19 +0100

    Add function that change the headline for every pages.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom'; // Import useLocation
 import SearchIcon from '@mui/icons-material/Search';
 import StarBorderPurple500Icon from '@mui/icons-material/StarBorderPurple500';
 
 const Navbar = () => {
+  const location = useLocation(); // Use useLocation hook to get the current location
+
+  // Determine the headline based on the current location
+  const getHeadline = () => {
+    if (location.pathname.includes('/explore')) { // Check if the pathname includes 'explore'
+      return 'Explore';
+    } else if (location.pathname.includes('/bookmark')) { // Check if the pathname includes 'bookmark'
+      return 'Bookmark';
+    } else if (location.pathname.includes('/profile')) { // Check if the pathname includes 'bookmark'
+        return 'Profile';
+    } else if (location.pathname.includes('/messages')) { // Check if the pathname includes'messages'
+        return 'Messages';
+    }
+    return 'Home'; // Default to 'Home' if neither
+  };
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-4 my-5 justify-center'>
        <div className='mx-auto md:mx-0'>
             <img 
                 src="/X-Logo.png" 
-                alt="twitter logo" 
+                alt="logo" 
                 width={"40px"} 
                 className='ml-8'
             />
@@ -16,11 +33,10 @@ const Navbar = () => {
 
        <div className='col-span-2 md:border-x-2 mb:border-slate-200 md:px-6 my-6 md:my-0'>
             <div className='flex justify-between items-center'>
-                <h2 className='font-bold text-2xl'>Home</h2>
+                <h2 className='font-bold text-2xl'>{getHeadline()}</h2> {/* Dynamically set the headline */}
                 <StarBorderPurple500Icon />
             </div>
        </div>
-
 
         <div className='px-0 md:px-6 mx-auto'>
             <SearchIcon className='absolute m-2'/>
@@ -30,4 +46,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
