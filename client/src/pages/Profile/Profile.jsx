4ac60db326fcda3b1b9f7de363d4b013078f44d2
Caleb ESHO <110430708+the-eshocaleb@users.noreply.github.com>
2024-02-18 12:42:02 +0100

    implement follow and unfollow profile

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -19,6 +19,7 @@ const Profile = () => {
   const [activeTab, setActiveTab] = useState("posts");
 
   const [openModal, setOpenModal] = useState(false);
+  
 
   const { currentUser } = useSelector((state) => state.user);
   const { username } = useParams();
@@ -91,57 +92,75 @@ const Profile = () => {
         </div>
 
         <div className="feed col-span-2 border-x-2 border-t-slate-800 px-6 overflow-y-auto h-screen pt-[70px]">
-          <div className="flex justify-between items-center">
-            <img
-              src={userProfile && userProfile.profilePicture}
-              alt="Profile Picture"
-              className="w-20 h-20 rounded-full object-cover"
-            />
-
-            {/* User details */}
-            <div>
-              <h1 className="text-2xl font-bold">
-                {userProfile && userProfile.firstname}{" "}
-                {userProfile && userProfile.lastname}
-              </h1>
-              <span className="text-gray-500">
-                @{userProfile && userProfile.username}
-              </span>
-              <p className="text-gray-500">{userProfile && userProfile.bio}</p>
-              <p>{userProfile && userProfile.following.length}</p>
-              <p>{userProfile && userProfile.followers.length}</p>
-            </div>
-
-            {/* If profile page is for the current user, show edit profile button */}
-            {currentUser.username === username ? (
-              <button
-                className="bg-blue-500 text-white rounded-full px-4 py-2"
-                onClick={() => setOpenModal(true)}
-              >
-                Edit Profile
-              </button>
-            ) : // If the current user is following the profile user, show "Following" button
-            currentUser.following.includes(username) ? (
-              <button
-                className="bg-blue-500 text-white rounded-full p-2"
-                onClick={handleFollow}
-              >
-                Following
-              </button>
+          <div className="relative p-4">
+            {/* Background Image Placeholder */}
+            <div className="absolute top-0 left-0 w-full max-h-48">
+            {userProfile && userProfile.bannerPicture ? (
+              <img
+                src={userProfile.bannerPicture}
+                alt="Banner"
+                className="max-h-48 w-full object-cover"
+              />
             ) : (
-              // Otherwise, show "Follow" button
-              <button
-                className="bg-blue-500 text-white rounded-full p-2"
-                onClick={handleFollow}
-              >
-                Follow
-              </button>
-            )}
+              <div className="w-full h-48 bg-gray-400"></div>
+            )}  
           </div>
 
-          <div className="profile-tabs border-b border-gray-300">
+
+            {/* Container for the content below the background */}
+            <div className="relative mt-28 pb-4"> 
+              {/* Profile Image */}
+              <div className="flex justify-center md:justify-start -mt-2">
+                <img
+                  src={userProfile && userProfile.profilePicture ? userProfile.profilePicture : "https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png"}
+                  alt="Profile"
+                  className="w-32 h-32 rounded-full object-cover border-4 border-white bg-white"
+                />
+              </div>
+
+              {/* Container for Information and Button */}
+              <div className="mt-2 md:flex md:justify-between md:items-end">
+                {/* User Info */}
+                <div className="text-center md:text-left">
+                  <h1 className="text-2xl font-bold">
+                    {userProfile && userProfile.firstname} {userProfile && userProfile.lastname}
+                  </h1>
+                  <span className="text-gray-500">@{userProfile && userProfile.username}</span>
+                  <p className="text-gray-500">{userProfile && userProfile.bio}</p>
+                  <div className="flex justify-center space-x-4 md:justify-start">
+                    <p><span className="font-bold">{userProfile && userProfile.following.length}</span> Following</p>
+                    <p><span className="font-bold">{userProfile && userProfile.followers.length}</span> Followers</p>
+                  </div>
+                </div>
+
+              {/* Action Button */}
+              <div className="mt-4 md:mt-0">
+                {currentUser.username === username ? (
+                  <button
+                    className="bg-blue-500 text-white rounded-full px-4 py-2"
+                    onClick={() => setOpenModal(true)}
+                  >
+                    Edit Profile
+                  </button>
+                ) : currentUser.following.includes(username) ? (
+                  <button className="bg-blue-500 text-white rounded-full px-4 py-2"
+                   onClick={handleFollow}>
+                    Following
+                  </button>
+                ) : (
+                  <button className="bg-blue-500 text-white rounded-full px-4 py-2"
+                  onClick={handleFollow}>
+                    Follow
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+          <div className="profile-tabs border-b-2 border-gray-300">
             {/* Tabs for "Posts", "Media", "Likes" */}
-            <div className="flex justify-around text-sm font-medium text-gray-500">
+            <div className="flex justify-around text-sm font-medium text-gray-500 mb-10 border-b-2">
               <button
                 className={`px-4 py-2 ${
                   activeTab === "posts"
@@ -174,77 +193,74 @@ const Profile = () => {
               </button>
             </div>
 
-            {/* Content below the tabs */}
-            <div>
-              {activeTab === "posts" && (
-                <div className="posts">
-                  {/* Check if userTweets is not null and has length */}
-                  {userTweets && userTweets.length > 0 ? (
-                    userTweets.map((tweet) => (
-                      <div key={tweet._id}>
-                        <Tweet tweet={tweet} setData={setUserTweets} />
-                      </div>
-                    ))
-                  ) : (
-                    // Display message if there are no tweets
-                    <p className="text-center text-gray-600 mt-10">
-                      No posts found.
-                    </p>
-                  )}
-                </div>
-              )}
-
-              {/* USER MEDIA */}
-              {activeTab === "media" && (
+              {/* Content below the tabs */}
+              <div>
+                {activeTab === 'posts' && (
+                  <div className="posts">
+                    {/* Map through posts and render them */}
+                    {userTweets &&
+                      userTweets.map((tweet) => (
+                        <div key={tweet._id}>
+                          <Tweet tweet={tweet} setData={setUserTweets} />
+                        </div>
+                      ))}
+                  </div>
+                )}
+                {/* USER MEDIA */}
+                {activeTab === 'media' && (
                 <div className="flex flex-wrap justify-center md:justify-start">
                   {userMedia && userMedia.length > 0 ? (
-                    userMedia.map((mediaItem, index) => (
-                      <div key={index} className="w-1/3 p-1">
-                        <img
-                          src={mediaItem}
-                          alt={`Media ${index + 1}`}
-                          className="w-full h-auto aspect-square object-cover"
-                        />
-                      </div>
-                    ))
+                    userMedia.map((mediaItem, index) => {
+                      // Determine if the media item is a video by looking for video file extensions in the URL
+                      const isVideo = mediaItem.match(/\.(mp4|mov|avi|wmv|flv|mkv)(\?alt=media&token=[\w-]+)?$/i);
+
+                      return (
+                        <div key={index} className="w-1/3 p-1">
+                          {isVideo ? (
+                            // Render a video element for video URLs
+                            <video controls className="w-full h-auto aspect-square object-cover">
+                              <source src={mediaItem} type={`video/${mediaItem.split('.').pop().split('?')[0]}`} />
+                              Your browser does not support the video tag.
+                            </video>
+                          ) : (
+                            // Render an image element for image URLs
+                            <img src={mediaItem} alt={`Media ${index + 1}`} className="w-full h-auto aspect-square object-cover" />
+                          )}
+                        </div>
+                      );
+                    })
                   ) : (
-                    <p className="text-center text-gray-600 mt-10">
-                      No media found.
-                    </p>
+                    <p>No media found.</p>
                   )}
                 </div>
               )}
 
-              {/* USER LIKES */}
-              {activeTab === "likes" && (
-                <div className="likes">
-                  {/* Check if userLikes is not null and has length */}
-                  {userLikes && userLikes.length > 0 ? (
-                    userLikes.map((tweet) => (
-                      <div key={tweet._id}>
-                        <Tweet tweet={tweet} setData={setUserLikes} />
-                      </div>
-                    ))
-                  ) : (
-                    // Display message if there are no liked tweets
-                    <p className="text-center text-gray-600 mt-10">
-                      No likes found.
-                    </p>
-                  )}
-                </div>
-              )}
+
+                {activeTab === 'likes' && (
+                  <div className="likes">
+                    {/* Map through likes and render them */}
+                    {userLikes &&
+                      userLikes.map((tweet) => (
+                        <div key={tweet._id}>
+                          <Tweet tweet={tweet} setData={setUserLikes} />
+                        </div>
+                      ))}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
+  
+          <div className="px-6 pt-[70px]">
+            <RightSideBar />
+          </div>
         </div>
-        <div className="px-6 pt-[70px]">
-          <RightSideBar />
-        </div>
-      </div>
-
-      {/* EDIT PROFILE MODAL */}
-      {openModal && <EditProfile setOpen={setOpenModal} />}
-    </>
-  );
-};
-
-export default Profile;
+  
+        {/* EDIT PROFILE MODAL */}
+        {openModal && <EditProfile setOpen={setOpenModal} />}
+      </>
+    );
+  };
+  
+  export default Profile;
+  
