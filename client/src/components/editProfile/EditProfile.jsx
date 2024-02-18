5980057e58f39e7731f5a1e2a8981d9a8270db8d
ven2604 <khanhnguyen26403@gmail.com>
2024-02-18 11:31:32 +0100

    Fix Bug and add BannerPicture

--- client/src/components/editProfile/EditProfile.jsx
+++ client/src/components/editProfile/EditProfile.jsx
@@ -1,10 +1,6 @@
+// EditProfile.jsx
 import React, { useEffect, useState } from "react";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "firebase/storage";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import app from "../../firebase";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
@@ -12,104 +8,110 @@ import { updateProfile, logout } from "../../redux/userSlice";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import { useNavigate } from "react-router-dom";
 
-
 const EditProfile = ({ setOpen }) => {
-  const [img, setImg] = useState(null);
   const [file, setFile] = useState(null);
   const [imgUploadProgress, setImgUploadProgress] = useState(0);
-  // const [banner, setBanner] = useState(null);
-  // const [bannerUploadProgress, setBannerUploadProgress] = useState(0);
+  const [bannerFile, setBannerFile] = useState(null);
+  const [bannerUploadProgress, setBannerUploadProgress] = useState(0);
   const [bio, setBio] = useState("");
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
-  const { currentUser } = useSelector((state) => state.user);
-
+  const currentUser = useSelector((state) => state.user.currentUser);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  // handle user update
-  const updateUser = async () => {
+  const updateUser = async (profilePictureUrl, bannerPictureUrl) => {
     try {
-      const updateUserProfile = await axios.put(`/users/${currentUser.username}`, {
-        profilePicture: img,
-        firstname: firstname,
-        lastname: lastname,
-        bio: bio,
-      });
-      dispatch(updateProfile(updateUserProfile));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const uploadImg = (file) => {
-    const storage = getStorage(app);
-    const fileName = new Date().getTime() + file.name;
-    const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setImgUploadProgress(Math.round(progress));
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-
-          default:
-            break;
+      const updatedUserResponse = await axios.put(
+        `/users/${currentUser.username}`,
+        {
+          profilePicture: profilePictureUrl,
+          bannerPicture: bannerPictureUrl, // Include bannerPicture in the update
+          firstname: firstname,
+          lastname: lastname,
+          bio: bio,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${currentUser.token}`, // Adjust based on how you store and use tokens
+          },
         }
-      },
-      (error) => {},
-      () => {
-        // Upload completed successfully, now we can get the download URL and update the state
-        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-          setImg(downloadURL);
-          
-        });
-      }
-    );
-  };
-
-  const handleDeleteUser = async () => {
-    try {
-      const deleteUser = await axios.delete(`/users/${currentUser.username}`);
-      dispatch(logout());
-      navigate("/login");
-      console.log(deleteUser);
+      );
+  
+      const updatedUserData = updatedUserResponse.data;
+      dispatch(
+        updateProfile({
+          profilePicture: updatedUserData.profilePicture,
+          bannerPicture: updatedUserData.bannerPicture, // Dispatch bannerPicture update
+          firstname: updatedUserData.firstname,
+          lastname: updatedUserData.lastname,
+          bio: updatedUserData.bio,
+        })
+      );
+  
+      setOpen(false);
+      // Optionally, navigate to the updated profile or show a success message
     } catch (error) {
-      console.log(error);
+      console.error("Failed to update user profile:", error);
+      // Handle the error, e.g., through a notification to the user
     }
-  }
+  };  
 
-  //upload file to firebase storage when file state changes
-  // get the download url and update the state
-  useEffect(() => {
-    file && uploadImg(file);
-  }, [file]);
+  const handleSave = async () => {
+    let profilePictureUrl = currentUser.profilePicture;
+    let bannerPictureUrl = currentUser.bannerPicture; // Assuming you have a bannerPicture field in your user model
+  
+    if (file) {
+      profilePictureUrl = await uploadImg(file, setImgUploadProgress); // Modify uploadImg to accept a progress setter function
+    }
+  
+    if (bannerFile) {
+      bannerPictureUrl = await uploadImg(bannerFile, setBannerUploadProgress); // Reuse uploadImg for banner file upload
+    }
+  
+    // After getting the URLs or if there's no file, call updateUser
+    updateUser(profilePictureUrl, bannerPictureUrl);
+  };
+  
+
+  const uploadImg = (file, setUploadProgress) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name; // Ensure unique file name
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+  
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setUploadProgress(Math.round(progress)); // Update progress
+        },
+        (error) => {
+          console.error("Image upload error:", error);
+          reject(error);
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL); // Resolve the promise with the download URL
+          });
+        }
+      );
+    });
+  };
+  
 
   return (
     <div className="absolute w-full h-full top-0 left-0 bg-blur flex items-center justify-center pt-[100px]">
-      <div className="relative w-[500px] h-[500px] bg-slate-200 rounded-lg p-8 flex flex-col gap-4">
-        <button
-          className="absolute top-7 right-3 cursor-pointer text-red-500"
-          onClick={() => setOpen(false)}
-        >
+      <div className="relative w-[500px] h-[600px] bg-slate-200 rounded-lg p-8 flex flex-col gap-4">
+        <button className="absolute top-7 right-3 cursor-pointer text-red-500" onClick={() => setOpen(false)}>
           <CloseRoundedIcon />
         </button>
-
         <h1 className="text-2xl font-bold">Edit Profile</h1>
-
         <p>Upload profile picture</p>
-        {imgUploadProgress > 0 ? "Uploading " + imgUploadProgress + "%" : null}
+        {imgUploadProgress > 0 && <p>Uploading profile picture: {imgUploadProgress}%</p>}
         <input
           type="file"
           className="bg-transparent border border-slate-500 rounded p-2"
@@ -117,45 +119,41 @@ const EditProfile = ({ setOpen }) => {
           onChange={(e) => setFile(e.target.files[0])}
         />
 
+        <p>Upload banner picture</p>
+        {bannerUploadProgress > 0 && <p>Uploading banner picture: {bannerUploadProgress}%</p>}
+        <input
+          type="file"
+          className="bg-transparent border border-slate-500 rounded p-2"
+          accept="image/*"
+          // Use setBannerFile to store the selected file for the banner
+          onChange={(e) => setBannerFile(e.target.files[0])}
+        />
+
         <input
           type="text"
           className="bg-transparent border border-slate-500 rounded p-2"
-          placeholder={currentUser.firstname}
-          required
+          placeholder="Firstname"
+          value={firstname}
           onChange={(e) => setFirstname(e.target.value)}
         />
         <input
           type="text"
           className="bg-transparent border border-slate-500 rounded p-2"
-          placeholder={currentUser.lastname}
-          required
+          placeholder="Lastname"
+          value={lastname}
           onChange={(e) => setLastname(e.target.value)}
         />
-
-        <input
-          type="text"
+        <textarea
           className="bg-transparent border border-slate-500 rounded p-2"
-          placeholder={currentUser.bio}
+          placeholder="Bio"
+          value={bio}
           onChange={(e) => setBio(e.target.value)}
         />
-
         <div className="flex justify-end">
-          <button
-            className="bg-blue-500 text-white rounded-full px-4 py-2"
-            onClick={updateUser}
-          >
+          <button className="bg-blue-500 text-white rounded-full px-4 py-2" onClick={handleSave}>
             Save
           </button>
         </div>
-        {/* <div className="flex justify-end">
-          <button
-            className="bg-red-500 text-white rounded-full px-4 py-2"
-            onClick={handleDeleteUser}
-          >
-            Delete account
-          </button>
-        </div> */}
-        
       </div>
     </div>
   );
