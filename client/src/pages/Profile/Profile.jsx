779fec48fa085d0c96bdade738becec69c44f9b3
ven2604 <khanhnguyen26403@gmail.com>
2024-02-18 22:24:03 +0100

    Add handle errors

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -104,6 +104,7 @@ const Profile = () => {
             ) : (
               <div className="w-full h-48 bg-gray-400"></div>
             )}  
+          </div>
 
 
             {/* Container for the content below the background */}
@@ -142,11 +143,12 @@ const Profile = () => {
                     Edit Profile
                   </button>
                 ) : currentUser.following.includes(username) ? (
-                  <button className="bg-blue-500 text-white rounded-full px-4 py-2">
+                  <button className="bg-blue-500 text-white rounded-full px-4 py-2"
+                  onClick={handleFollow}>
                     Following
                   </button>
                 ) : (
-                  <button className="bg-blue-500 text-white rounded-full px-4 py-2">
+                  <button className="bg-blue-500 text-white rounded-full px-4 py-2" onClick={handleFollow}>
                     Follow
                   </button>
                 )}
@@ -211,7 +213,6 @@ const Profile = () => {
                       // Determine if the media item is a video by looking for video file extensions in the URL
                       const isVideo = mediaItem.match(/\.(mp4|mov|avi|wmv|flv|mkv)(\?alt=media&token=[\w-]+)?$/i);
 
-
                       return (
                         <div key={index} className="w-1/3 p-1">
                           {isVideo ? (
@@ -232,6 +233,7 @@ const Profile = () => {
                   )}
                 </div>
               )}
+
 
                 {activeTab === 'likes' && (
                   <div className="likes">
