af96ccda19e246910ef1400e006772601050668d
Caleb ESHO <110430708+the-eshocaleb@users.noreply.github.com>
2024-02-19 05:19:09 +0100

    update style comment and single tweet page

--- client/src/components/tweetAndReplies/TweetAndReplies.jsx
+++ client/src/components/tweetAndReplies/TweetAndReplies.jsx
@@ -1,41 +1,53 @@
-import React, {useState, useEffect} from 'react';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-import Tweet from '../tweet/Tweet';
-import { useParams } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import Tweet from "../tweet/Tweet";
+import { useParams } from "react-router-dom";
+import ReplyTweet from "../replyTweet/ReplyTweet";
 
 const TweetAndReplies = () => {
-    const [mainTweet, setMainTweet] = useState(null);
-    const {currentUser} = useSelector((state) => state.user);
-    const {id} = useParams();
+  const [mainTweet, setMainTweet] = useState(null);
+  const [replies, setReplies] = useState(null);
+  const { currentUser } = useSelector((state) => state.user);
+  const { id } = useParams();
 
-    // run this function anytime the user changes
-    useEffect(() => {
-        const fetchTweets = async () =>  {
-            try{
-                const tweets = await axios.get(`/tweets/find/${id}`);
-                setMainTweet(tweets.data)
-            } catch(err){
-                console.log(err)
-            }
-        };
-        fetchTweets();
-    }, [currentUser.username, id]);
+  // Fetch main tweet and replies when component mounts or currentUser changes
+  useEffect(() => {
+    const fetchTweetAndReplies = async () => {
+      try {
+        const response = await axios.get(`/tweets/find/${id}`);
+        const data = response.data;
 
-    console.log(mainTweet)
+        // Check if main tweet exists
+        if (data.length > 0) {
+          setMainTweet(data[0]);
 
-    
+          // Check if replies exist
+          if (data.length > 1) {
+            setReplies(data[1]);
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchTweetAndReplies();
+  }, [currentUser.username, id]);
 
   return (
-    <div className='mt-6'>
-        {mainTweet && mainTweet.map((tweet) => {
-            return (<div key={tweet._id} className='p-2'>
-                <Tweet tweet={tweet} setData={setMainTweet}/>     
-            </div>)
-        })}
+    <div className="mt-6">
+      {/* Render main tweet if it exists */}
+      {mainTweet && <Tweet tweet={mainTweet} />}
+
+      {/* Render ReplyTweet component if mainTweet exists */}
+      {mainTweet && <ReplyTweet replyToId={id} />}
+
+      {/* Render replies if they exist */}
+      {replies &&
+        replies.map((reply) => <Tweet key={reply._id} tweet={reply} />)}
     </div>
-  )
-}
+  );
+};
 
 export default TweetAndReplies;
