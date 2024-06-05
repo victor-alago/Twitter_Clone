d8e1d7b46e3e54c6c2c9037637f99ebc8b2a8dc8
Duc-Tin-Nguyen <tinnd1506@gmail,com>
2024-06-05 21:18:43 +0200

    Updated the index with neo4j

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import http from "http"; // Add this line to import http module
+import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+import { neo4jDriver, mongoClient } from './db.js';
 
 // Import Routes
 import userRoutes from "./routes/users.js";
@@ -12,26 +13,41 @@ import authRoutes from "./routes/auths.js";
 import tweetRoutes from "./routes/tweets.js";
 
 const app = express();
-dotenv.config(); // get config for mongoDB
+dotenv.config(); // get config for MongoDB and Neo4j
 
-
-
-const connect = () => {
+// MongoDB connection
+const connectMongoDB = () => {
     mongoose.set("strictQuery", false);
     mongoose.connect(process.env.MONGO_URL).then(() => {
         console.log('Connected to MongoDB');
     }).catch((err) => {
+        console.error('Failed to connect to MongoDB', err);
         throw err;
     });
 };
 
+// Neo4j connection
+const connectNeo4j = async () => {
+    try {
+        await neo4jDriver.verifyConnectivity();
+        console.log('Connected to Neo4j');
+    } catch (err) {
+        console.error('Failed to connect to Neo4j', err);
+        throw err;
+    }
+};
+
+// Middleware
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+// Routes
 app.use("/server/users", userRoutes);
 app.use("/server/auth", authRoutes);
 app.use("/server/tweets", tweetRoutes);
 
+// Socket.io setup
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
@@ -39,7 +55,6 @@ const io = new Server(server, {
       methods: ["GET", "POST"],
     },
 });
-
 
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
@@ -56,11 +71,11 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
       console.log("User Disconnected", socket.id);
     });
-  });
-  
+});
 
-
-    server.listen(8000, () => {
-        connect();
-        console.log("SERVER IS RUNNING, PORT 8000");
-    });
+// Start server
+server.listen(8000, async () => {
+    connectMongoDB();
+    await connectNeo4j();
+    console.log("SERVER IS RUNNING, PORT 8000");
+});
